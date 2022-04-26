c17ee6070be336436958d35bcba591406f6a8ba4
Manishankar Vakta <manishankarvakta@gmail.com>
2022-04-26 22:44:22 +0600

    CRUD API endpoint for user

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 const port = process.env.PORT || 5000;
 
+
+
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -17,9 +22,57 @@ async function run() {
 
         const posDB = client.db('pos');
         const userCollection = posDB.collection('users');
-        const user = {name: "Manishankar Vakta", email: "manishankarvakta@gmail.com"};
-        const result = await userCollection.insertOne(user);
-        console.log(`User has been Created with user ID ${result.insertedId}`)
+
+        // get user
+        app.get('/user', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const user = await cursor.toArray();
+            res.send(user);
+        });
+
+        // get One User
+        app.get("/user/:id", async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+
+            const user = await userCollection.findOne(query);
+            res.send(user);
+        });
+
+
+        // update / put user
+        app.put('/user/:id', async(req,res)=>{
+            const id = req.params.id;
+            const user = req.body;
+            const filter = {_id: ObjectId(id)};
+
+            const updateUser = {
+                $set: user
+            }
+            
+            const option = {upsert:true};
+
+            const result = await userCollection.updateOne(filter, updateUser, option);
+            res.send(result);
+        })
+
+        // create user
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            console.log('create new user', user);
+            const result = await userCollection.insertOne(user);
+            res.send({ result: `success: ${result.insertedId}`, data: result });
+        })
+
+
+        // delete user
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
         // client.close();
@@ -28,8 +81,8 @@ async function run() {
 run().catch(console.dir);
 
 
-app.use(cors());
-app.use(express.json());
+
+
 
 app.get('/', (req, res) => {
     res.send('POS API');
