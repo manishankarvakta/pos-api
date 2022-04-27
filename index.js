9722bfff676961b59be3bb788d8bc0eb37935544
Manishankar Vakta <manishankarvakta@gmail.com>
2022-04-27 14:21:27 +0600

    Product CRUD API endpoint

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+const app = express();
 
+// Define port
 const port = process.env.PORT || 5000;
 
 
-
+// MiddleWare
 app.use(cors());
 app.use(express.json());
 
 
+// DATABASE CONNECTION
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
-const uri = "mongodb+srv://pos-user:TeDAD10lrudUgHxW@cluster0.rfbl8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rfbl8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -22,6 +25,7 @@ async function run() {
 
         const posDB = client.db('pos');
         const userCollection = posDB.collection('users');
+        const productCollection = posDB.collection('products');
 
         // get user
         app.get('/user', async (req, res) => {
@@ -62,7 +66,7 @@ async function run() {
             const user = req.body;
             console.log('create new user', user);
             const result = await userCollection.insertOne(user);
-            res.send({ result: `success: ${result.insertedId}`, data: result });
+            res.send(result.insertedId);
         })
 
 
@@ -73,6 +77,84 @@ async function run() {
             const result = await userCollection.deleteOne(query);
             res.send(result);
         })
+
+
+        /**
+         * PRODUCT CRUD API
+         * C - Create Products
+         * R - read products
+         * U - Update products
+         * D - Delete Products
+         */
+
+        //  * C - Create Products
+        app.post('/product', async (req, res) => {
+            const product = req.body;
+            console.log('create new product', product);
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        })
+        
+
+        /**
+         *  R - read 
+         * All products
+         * */
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const product = await cursor.toArray();
+            res.send(product);
+        });
+
+         /**
+         *  R - read 
+         * One product
+         * */
+          app.get("/product/:id", async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+
+
+        // U - Update products
+        app.put('/product/:id', async(req,res)=>{
+            const id = req.params.id;
+            const product = req.body;
+            const filter = {_id: ObjectId(id)};
+
+            const updateProduct = {
+                $set: product
+            }
+            
+            const option = {upsert:true};
+
+            const result = await productCollection.updateOne(filter, updateProduct, option);
+            res.send(result);
+        });
+
+        // create product
+        app.post('/product', async (req, res) => {
+            const product = req.body;
+            console.log('create new product', product);
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        })
+
+
+        // delete product
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
     }
     finally {
         // client.close();
